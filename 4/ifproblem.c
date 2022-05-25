5715be2359f6f1df9a61d16dea37099b10834f52
yingmuzhi <cyxscj@126.com>
2022-05-25 11:15:58 +0800

    mkdir 4

---
+++ 4/ifproblem.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(){
+    int i = 0;
+
+    if ( i == 0 )
+        if ( i != 0 ) {
+            printf("2\n");
+        }
+        else {
+            printf("3\n");
+        }
+
+    return 0;
+}
+
+
